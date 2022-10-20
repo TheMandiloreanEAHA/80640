1faa493faf2e40e968cf884a4eed0b0bb994fa33
TheMandiloreanEAHA <zs20018198@estudiantes.uv.mx>
2022-10-20 13:02:31 -0500

    Archivo java

--- MAVEN/practica06/src/main/java/mx/uv/c80640/App.java
+++ MAVEN/practica06/src/main/java/mx/uv/c80640/App.java
@@ -1,5 +1,8 @@
 package mx.uv.c80640;
 
+import static spark.Spark.*;
+
+
 /**
  * Hello world!
  *
@@ -9,5 +12,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        get("/hello", (req, res) -> "Hello World");
+        get("/alex", (req, res) -> "Hello Alex");
+        get("/Erick", (req, res) -> "Hello Erick");
+        get("/", (req, res) -> "<h1>Bienvenido</h1><img src='https://www.uv.mx/v2/images/logouv.jpg'>");
+
+
     }
 }
